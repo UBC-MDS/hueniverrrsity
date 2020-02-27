99b97b83279311d54365b4d87be5ca78e779c9ef
Simardeep Kaur <simardeep.kaur.jeji@gmail.com>
2020-02-26 20:10:12 -0700

    Submission

---
+++ R/theme_mcgill.R
@@ -0,0 +1,16 @@
+@author: Simardep kaur
+Implementation of theme_mcgill in hueniverrrsity
+"""
+
+#' Creating visualizations using the colours and fonts of visual identity of McGill University
+#'
+#' @param university
+#' @param
+#'
+#' @return Visualization with colours and fonts of visual identity of Mcgill University
+#'
+#'
+#' @examples
+theme_mcgill <- function(university){
+
+}
