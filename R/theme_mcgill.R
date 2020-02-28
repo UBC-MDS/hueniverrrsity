256bbf61256b7546b758c74dd95653335bc60ab0
Reiko Okamoto <reiko.okamoto17@gmail.com>
2020-02-28 10:50:39 -0800

    Edit UBC docstring, re-initialize McGill docstring to be consistent with others

---
+++ R/theme_mcgill.R
@@ -0,0 +1,24 @@
+#' McGill University Plotting Theme
+#'
+#' @param colour_use either 'fill' or 'colour' depending on how the colour will be displayed.
+#'
+#' @description: theme_ubc() creates a theme using the University of British Columbia's visual identity
+#' to be used with ggplot2. See the isual identity at https://brand.ubc.ca/guidelines/downloads/ubc-colour-and-fonts/
+#'
+#' @usage: ggplot(data, aes(x, y, colour = class)) + theme_ubc('colour')
+#'
+#' @return reformatted plot with UBC's visual identity theme.
+#' @export
+#'
+#' @examples
+#'
+#' \dontrun{ggplot(students_admissions, aes(x = high_gpa, y = department, fill = as.factor(department)))
+#'      + geom_bar(stat = 'identity')
+#'      + theme_ubc(colour_use = 'fill')}
+#'
+#' \dontrun{ggplot(students_admissions, aes(x = high_gpa, y = department, colour = as.factor(department)))
+#'      + geom_bar(stat = 'identity')
+#'      + theme_ubc(colour_use = 'colour')}
+theme_ubc <- function(colour_use) {
+
+}
--- R/theme_ubc.R
+++ R/theme_ubc.R
@@ -1,25 +1,24 @@
 #' University of British Columbia Plotting Theme
 #'
-#' @param colour_use either 'fill' or 'color' depending on how the colour will be displayed.
-#' @param colour_palette either 'vibrant', 'cool' or 'awards' depending on the use of the visualization.
-#' 
-#' @description: theme_UBC() creates a theme using the University of British Columbia visual identity
-#' to be used with ggplot2. See UBC Visual identity at https://brand.ubc.ca/guidelines/downloads/ubc-colour-and-fonts/
-#' 
-#' @usage: ggplot(data, aes(variable x, variable y, colour = class)) + theme_UBC('colour', 'vibrant')
+#' @param colour_use either 'fill' or 'colour' depending on how the colour will be displayed.
 #'
-#' @return reformatted plot for with UBC visual identity theme. 
+#' @description: theme_ubc() creates a theme using the University of British Columbia's visual identity
+#' to be used with ggplot2. See the isual identity at https://brand.ubc.ca/guidelines/downloads/ubc-colour-and-fonts/
+#'
+#' @usage: ggplot(data, aes(x, y, colour = class)) + theme_ubc('colour')
+#'
+#' @return reformatted plot with UBC's visual identity theme.
 #' @export
 #'
 #' @examples
-#' 
-#' \dontrun{ggplot(students_admissions, aes(x = high_gpa, y = department, fill = as.factor(department))) 
-#'      + geom_bar(stat = 'identity') 
-#'      + theme_UBC(colour_type = 'fill', colour_palette = 'vibrant'}
 #'
-#' \dontrun{ggplot(students_admissions, aes(x = high_gpa, y = department, colour = as.factor(department))) 
-#'      + geom_bar(stat = 'identity')  
-#'      + theme_UBC(colour_type = 'colour', colour_palette = 'vibrant')}
-theme_ubc <- function() {
+#' \dontrun{ggplot(students_admissions, aes(x = high_gpa, y = department, fill = as.factor(department)))
+#'      + geom_bar(stat = 'identity')
+#'      + theme_ubc(colour_use = 'fill')}
+#'
+#' \dontrun{ggplot(students_admissions, aes(x = high_gpa, y = department, colour = as.factor(department)))
+#'      + geom_bar(stat = 'identity')
+#'      + theme_ubc(colour_use = 'colour')}
+theme_ubc <- function(colour_use) {
 
 }
