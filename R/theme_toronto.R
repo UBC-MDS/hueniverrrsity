7244d366313b927526500f81c84f937f3cb15f0e
moorhou7 <evelyn.moorhouse@mail.utoronto.ca>
2020-02-28 09:25:13 -0800

    added function after docstring

--- R/theme_toronto.R
+++ R/theme_toronto.R
@@ -1,16 +1,18 @@
-theme_toronto <- function(colour_use, colour_palette){
-#' @param colour_use either 'fill' or 'color' depending on how the colour will be displayed.
-#' @param colour_palette either 'vibrant' or 'cool' or 'awards' depending on the use of the visualization. 
+#' University of Toronto Plotting Theme
 #'
+#' @param colour_use either 'fill' or 'color' depending on how the colour will be displayed.
+#' @param colour_palette either 'vibrant', 'cool' or 'awards' depending on the use of the visualization.
+#' 
 #' @description: theme_toronto() creates a theme using the University of Toronto visual identity
 #' to be used with ggplot2
+#' 
+#' @usage: ggplot(data, aes(variable x, variable y, colour = class)) + theme_toronto('colour', 'vibrant')
 #'
-#' @usage: ggplot(data, aes(variable x, variable y, colour = class))  + theme_toronto('colour', 'vibrant')
-#'
-#' @details: Create your plot via ggplot2 and apply our theme to produce different type of U of T visual identity plots 
+#' @return reformatted plot for with U of T visual identity theme. 
 #' @export
-#' @examples
 #'
+#' @examples
+#' 
 #' \dontrun{ggplot(students_admissions, aes(x = high_gpa, y = department, fill = as.factor(department))) 
 #'      + geom_bar(stat = 'identity') 
 #'      + theme_toronto(colour_type = 'fill', colour_palette = 'vibrant'}
@@ -18,7 +20,5 @@ theme_toronto <- function(colour_use, colour_palette){
 #' \dontrun{ggplot(students_admissions, aes(x = high_gpa, y = department, colour = as.factor(department))) 
 #'      + geom_bar(stat = 'identity')  
 #'      + theme_toronto(colour_type = 'colour', colour_palette = 'vibrant')}
-#'
-#'
-#' @return reformatted plot for with U of T visual identity theme. 
+theme_toronto <- function(colour_use, colour_palette){
 }
